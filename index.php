ed982f68ed59b69a3975192439b0aa7f72455392
amaria-foukia <amaria.foukia@gmail.com>
2022-03-24 18:33:46 +0100

    adding a font

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSS -->
     <link href="style.css" rel="stylesheet" type="text/css" />
     <!-- CSS for Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Font Awesome" -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -76,8 +82,8 @@
 
 <!-- Large screens slider -->
 <section id="slider">
-    <h2 class="h3 mb-3">Quelques photos de nos magnifiques réalisations !</h2>
-    <p class="mb-3">Est sunt facilis sed molestias amet est autem eveniet aut nobis dicta </p>
+    <h2 class="h3">Quelques photos de nos magnifiques réalisations !</h2>
+    <p class="description">Est sunt facilis sed molestias amet est autem eveniet aut nobis dicta </p>
 
     <div class="slide content">
         <div id="lgCarousel" class="carousel carousel-dark slide" data-bs-ride="carousel">
@@ -130,8 +136,8 @@
 
 <!-- More description -->
 <section id="details">
-    <h2 class="h3 mb-3">Nos differents packs sur mesure !</h2>
-    <p class="mb-3">Est sunt facilis sed molestias amet est autem eveniet aut nobis dicta </p>
+    <h2 class="h3">Nos differents packs sur mesure !</h2>
+    <p class="description">Est sunt facilis sed molestias amet est autem eveniet aut nobis dicta </p>
 
     <div class="content-pack">
         <div class="pack mx-3">
--- result.php
+++ result.php
@@ -9,6 +9,10 @@
     <!-- CSS for Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
